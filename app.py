67c82e43b6921a09437355f312abf7443dfa89b0
aloagudelo <aloagudelo@gmail.com>
2023-07-17 22:04:42 -0500

    correcion para que corra las pruebas mientra habilito https

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from ApiCitas import app
 from waitress import serve
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=9000, url_scheme='https', certfile='/Apicitas/cert.pem', keyfile='/Apicitas/key.pem')
+    serve(app, host='0.0.0.0', port=9000)
