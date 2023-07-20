4bfb6f3104e6436a3c72d5194072afc85f00f54d
aloagudelo <aloagudelo@gmail.com>
2023-07-19 23:24:00 -0500

    se agrega la ruta para culta de medico disponible

--- ApiCitas.py
+++ ApiCitas.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pyodbc
 import ssl
-   
+
 app = Flask(__name__)
 CORS(app)
 
@@ -29,7 +29,6 @@ def get_cliente(cedula):
             "Entidad": result.Entidad,
             "ID": result.ID
         }
-       
         return jsonify(cliente)
     else:
         conn.close()
@@ -54,11 +53,53 @@ def consultar_cliente():
             }
             return jsonify(cliente)
         else:
-            return jsonify({"message": "usuarion o no tiene ficha creada en el hospital, es necesrio que se acerque personalmente a nuestras instalaciones a realizar la solicitud de cita"}), 404
+            return jsonify({"message": "Usuario no tiene ficha creada en el hospital. Es necesario que se acerque personalmente a nuestras instalaciones a realizar la solicitud de cita"}), 404
     else:
         return app.send_static_file('formulario.html')
 
+# Consultar médicos disponibles según el query proporcionado
+@app.route('/medicos_disponibles', methods=['GET'])
+def consultar_medicos_disponibles():
+    fecha = request.args.get('fecha')  # Obtener el valor de la variable "fecha" de la URL
+    
+    cursor = conn.cursor()
+    query = f"""
+        SELECT 
+            ISNULL(MED_NOMBRE, ' ') AS "NOMBRE PROFESIONAL",
+            (SELECT ESP_NOMBRE FROM TMESPECIALIDADES WHERE MED_ESPECIALIDAD_1 = ESP_COD) AS "NOMBRE ESPECIALIDAD",
+            ISNULL(TME2_FCH, 0) AS FECHA,
+            CAST(TME2_HH AS VARCHAR) + ':' + CAST(TME2_MM AS VARCHAR) AS "HORA",
+            ISNULL(TME2_COD, '') AS "CODIGO CLIENTE",
+            ISNULL((SELECT TOP 1 KC_NOM FROM TKCLIENTES WHERE TME2_ZONA = KC_ZONA AND TME2_COD = KC_COD), '') AS "NOMBRE CLIENTE",
+            TME2_ACTIVIDAD AS ACTIVIDAD
+        FROM TMTURNOSMEDICOSDETALLE
+        LEFT JOIN TMMEDICOS ON (TME2_CODM = MED_COD)
+        WHERE TME2_FCH = '{fecha}' AND TME2_ACTIVIDAD = 90
+    """
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+
+    if results:
+        medicos_disponibles = []
+        for result in results:
+            # Convertir el resultado en un diccionario para acceder a los campos por nombre
+            medico = {
+                "NOMBRE PROFESIONAL": result[0],
+                "NOMBRE ESPECIALIDAD": result[1],
+                "FECHA": result[2],
+                "HORA": result[3],
+                "CODIGO CLIENTE": result[4],
+                "NOMBRE CLIENTE": result[5],
+                "ACTIVIDAD": result[6]
+            }
+            medicos_disponibles.append(medico)
+
+        return jsonify(medicos_disponibles)
+    else:
+        return jsonify({"message": "No hay médicos disponibles"}), 404
+
 if __name__ == '__main__':
-       context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-       context.load_cert_chain('cert.pem', 'key.pem')
-       app.run(debug=True, host='0.0.0.0', port=9000, ssl_context=context)
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain('cert.pem', 'key.pem')
+    app.run(debug=True, host='0.0.0.0', port=9000, ssl_context=context)
